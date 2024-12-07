d86d6b1b6b23082c56482d230f5c070369d9a01d
fff-12 <161848104+fff-12@users.noreply.github.com>
2024-12-07 17:00:29 +0200

    Update p.py

--- p.py
+++ p.py
@@ -6,9 +6,10 @@ current_year = 2024
 
 # Гіпотеза 1: Рейтинг компаній до 2000 року vs після 2000 року
 data['before_2000'] = data['Founded'] < 2000
+data['after_2000'] = data['Founded'] > 2000
 
 avg_Rating_before_2000 = data[data['before_2000']]['Rating'].mean()
-avg_Rating_after_2000 = data[~data['before_2000']]['Rating'].mean()
+avg_Rating_after_2000 = data[data['after_2000']]['Rating'].mean()
 
 print("Гіпотеза 1:")
 print(f"Середній рейтинг компаній, створених до 2000 року: {avg_Rating_before_2000:.2f}")
@@ -18,14 +19,14 @@ if avg_Rating_before_2000 > avg_Rating_after_2000:
 else:
     print("Гіпотеза спростована: новіші компанії мають вищий рейтинг.")
 
-# Гіпотеза 2: Рейтинг компаній залежить від їх віку
+# Гіпотеза 2: Рейтинг компаній зменшується з віком
 data['company_age'] = current_year - data['Founded']
 
 data['age_group'] = (data['company_age'] // 10) * 10
 
 age_group_Rating = data.groupby('age_group')['Rating'].mean()
 
-print("\nГіпотеза 2:")
+print("Гіпотеза 2:")
 print("Середній рейтинг за віковими групами:")
 print(age_group_Rating)
 
